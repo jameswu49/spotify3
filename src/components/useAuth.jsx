f80dac4ebafa4f957fc5ba9e5cd8bf6a7be45ebf
Jimmy Wu <jameswu49@gmail.com>
2023-03-30 22:33:17 -0700

    changed request

--- src/components/useAuth.jsx
+++ src/components/useAuth.jsx
@@ -6,45 +6,23 @@ export default function useAuth(code) {
     const [refreshToken, setRefreshToken] = useState()
     const [expiresIn, setExpiresIn] = useState()
 
-    useEffect(() => {
-        if (!code) return
-        axios
-            .post('https://lofi-player.herokuapp.com/login', {
-                code,
-            })
-            .then(res => {
-                setAccessToken(res.data.accessToken)
-                setRefreshToken(res.data.refreshToken)
-                setExpiresIn(res.data.expiresIn)
-                window.history.pushState({}, null, "/")
-            })
-            .catch((err) => {
-                // window.location = "/"
-                console.log(err.response)
-            })
-    }, [code])
-
     // useEffect(() => {
-    //     if (!refreshToken || !expiresIn) return
-    //     const interval = setInterval(() => {
-    //         axios
-    //             .post('https://lofi-player.herokuapp.com/refresh', {
-    //                 refreshToken,
-    //             })
-    //             .then(res => {
-    //                 setAccessToken(res.data.accessToken)
-    //                 setExpiresIn(res.data.expiresIn)
-    //                 window.history.pushState({}, null, "/")
-    //             })
-    //             .catch((err) => {
-    //                 // window.location = "/"
-    //                 console.log(err.response)
-    //             })
-    //     }, (expiresIn - 60) * 1000)
-
-    //     return () => clearInterval(interval)
-
-    // }, [refreshToken, expiresIn])
+    //     if (!code) return
+    //     axios
+    //         .post('https://lofi-player.herokuapp.com/login', {
+    //             code,
+    //         })
+    //         .then(res => {
+    //             setAccessToken(res.data.accessToken)
+    //             setRefreshToken(res.data.refreshToken)
+    //             setExpiresIn(res.data.expiresIn)
+    //             window.history.pushState({}, null, "/")
+    //         })
+    //         .catch((err) => {
+    //             // window.location = "/"
+    //             console.log(err.response)
+    //         })
+    // }, [code])
 
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
@@ -62,6 +40,27 @@ export default function useAuth(code) {
             });
     }, []);
 
+    useEffect(() => {
+        if (!refreshToken || !expiresIn) return
+        const interval = setInterval(() => {
+            axios
+                .post('https://lofi-player.herokuapp.com/refresh', {
+                    refreshToken,
+                })
+                .then(res => {
+                    setAccessToken(res.data.accessToken)
+                    setExpiresIn(res.data.expiresIn)
+                    window.history.pushState({}, null, "/")
+                })
+                .catch((err) => {
+                    // window.location = "/"
+                    console.log(err.response)
+                })
+        }, (expiresIn - 60) * 1000)
+
+        return () => clearInterval(interval)
+
+    }, [refreshToken, expiresIn])
 
     return accessToken
 }
