ab7645a5b31cf015d787d399e8e2c5dd160e1ffe
Jimmy Wu <jameswu49@gmail.com>
2023-03-31 23:01:49 -0700

    testing

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -4,7 +4,7 @@ export default function Login() {
     return (
         <div className="flex justify-center items-center h-screen">
             <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
-                <a href="http://localhost:3001/login">Log in to Spotify</a>
+                <a href="https://lofi-player.herokuapp.com/login">Log in to Spotify</a>
             </button>
         </div >
     )
