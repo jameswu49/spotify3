6384f216bfe6cd5b633fc3fbb53760e3e4a25d40
Jimmy Wu <jameswu49@gmail.com>
2023-04-11 17:16:13 -0700

    secret

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ require('dotenv').config({ path: '../.env' });
 
 
 const client_id = "641ca7a434204e509b4c51ac6b20bd7b"
-const client_secret = process.env.SPOTIFY_CLIENT_SECRET
+const client_secret = '9c714bc6db70462f9f637535311e8a8c'
 const redirect_uri = "https://lofi-player.herokuapp.com/callback";
 
 /**
