3a6de8bf1e32beb1f62de66a58fdb767eea05486
Jimmy Wu <jameswu49@gmail.com>
2023-04-12 15:05:56 -0700

    changed localhost to heroku

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ require('dotenv').config({ path: '../.env' });
 
 const client_id = process.env.SPOTIFY_CLIENT_ID
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET
-const redirect_uri = 'http://localhost:3001/callback'
+const redirect_uri = 'https://lofi-player.herokuapp.com/' || 'http://localhost:3001/callback'
 
 /**
  * Generates a random string containing numbers and letters
@@ -65,7 +65,7 @@ app.get('/callback', function (req, res) {
     const storedState = req.cookies ? req.cookies[stateKey] : null;
 
     if (state === null || state !== storedState) {
-        res.redirect('http://localhost:3000/callback' +
+        res.redirect('https://lofi-player.herokuapp.com/callback' +
             querystring.stringify({
                 error: 'state_mismatch'
             }));
@@ -102,13 +102,13 @@ app.get('/callback', function (req, res) {
                 });
 
                 // we can also pass the token to the browser to make requests from there
-                res.redirect('http://localhost:3000/callback#' +
+                res.redirect('https://lofi-player.herokuapp.com/callback#' +
                     querystring.stringify({
                         access_token: access_token,
                         refresh_token: refresh_token
                     }));
             } else {
-                res.redirect('http://localhost:3001/#' +
+                res.redirect('https://lofi-player.herokuapp.com/#' +
                     querystring.stringify({
                         error: 'invalid_token'
                     }));
